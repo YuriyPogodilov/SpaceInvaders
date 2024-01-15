5c7548ff2352f64e329fd5a1c340ca948c4061cb
Yuriy <aka4an@gmail.com>
2024-01-15 23:08:44 +0700

    Updated speed

--- spaceship/spaceship.gd
+++ spaceship/spaceship.gd
@@ -1,6 +1,6 @@
 class_name Spacesheep extends CharacterBody2D
 
-@export_range(50, 1000, 500) var SPEED = 600
+@export_range(50, 1000, 50) var SPEED = 600
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
